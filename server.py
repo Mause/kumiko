9b7e9c4138473d48c32caab6c6b654bd88910a93
Elliana May <me@mause.me>
2026-04-08 19:08:23 +0800

    update imports

--- server.py
+++ server.py
@@ -4,7 +4,7 @@ import os
 import json
 from bottle import route, run, request, static_file, abort
 from kumikolib import Kumiko
-from lib.html import HTML
+from kumiko.html import HTML
 
 static_files = {
 	'jquery-3.2.1.min.js': True,
